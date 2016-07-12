e9f9bd3d669d9a236e9f68562373af0f65c55440
czumczum <e.mindjunk@gmail.com>
2016-07-12 15:02:33 +0200

    Menu for wide screens & page loading

--- app.js
+++ app.js
@@ -1,30 +1,195 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     //Variables
-    var showLetter;
-    var letters = document.querySelectorAll(".abc i");
+    var letterSpace = document.querySelector(".abc");
     var bigLetter = document.querySelector('h1.letter');
-    var count = 0;
+    var navMenu = document.querySelectorAll('nav h1');
     var letterMax = 10; //The user set the max length of letters in one word
 
 
     //Functions
-    showLetter = function (e) {
-        if (letters[count].classList.contains('is-paused')){
-            letters[count].classList.remove('is-paused');
+    var loadPage = function (el) {
+        console.log(el);
+        bigLetter.innerText = el;
+        var showLetter;
+        var counter = 0;
+        var toRemove = document.querySelectorAll('.abc p');
+        if (toRemove != []) {
+            for (var i = 0; i < toRemove.length; i++) {
+                toRemove[i].parentNode.removeChild(toRemove[i]);
+            }
         }
-        count++;
-        if (count == letters.length) {
-            bigLetter.removeEventListener("click", showLetter)
+        for (key in lettersDb[el]) {
+            var icon = document.createElement("i");
+            icon.classList.add(lettersDb[el][key]);
+            icon.classList.add("hidden");
+            icon.classList.add("is-paused");
+            var paragraf = document.createElement("p");
+            paragraf.appendChild(icon);
+            letterSpace.appendChild(paragraf);
+            icon.dataset.key = key;
+            counter++;
         }
+        var letters = document.querySelectorAll(".abc i");
+        var count = 0;
+        showLetter = function (e) {
+            if (letters[count].classList.contains('is-paused')) {
+                letters[count].classList.remove('is-paused');
+            }
+            count++;
+            if (count == letters.length) {
+                bigLetter.removeEventListener("click", showLetter)
+            }
+        };
+        bigLetter.addEventListener("click", showLetter);
     };
-    
-    bigLetter.addEventListener("click", showLetter);
 
-    for (var i = 0; i < letters.length; i++) {
-        console.log(letters[i].dataset.key);
+    for (var i = 0; i < navMenu.length; i++) {
+        navMenu[i].addEventListener("click", function() {
+            var letter = this.innerText.toLowerCase();
+            loadPage(letter);
+            console.log(typeof letter);
+            console.log(letter);
+        });
     }
     
-    
     //Database
+    var lettersDb =
+    {
+        "a": {
+        "aktor": "flaticon-charlie-chaplin",
+            "auto": "flaticon-car",
+            "arena": "flaticon-big-stadium",
+            "ala": "flaticon-girl",
+            "afryka": "flaticon-africa-map",
+            "ara": "flaticon-macaw",
+            "autobus": "flaticon-bus"
+    },
+        "b": {
+        "balon": "flaticon",
+            "byk": "flaticon",
+            "bocian": "brat",
+            "brat": "flaticon",
+            "biurko": "flaticon-desk",
+            "bank": "flaticon-bank",
+            "but": "flaticon-cinderella-shoe"
+    },
+        "c": {
+        "cyrk": "flaticon-circus",
+            "cyklop": "flaticon-cyclops"
+    },
+        "cz": {
+        "czapka": "flaticon-mortarboard"
+    },
+        "d": {
+        "dym": "flaticon-nature",
+    },
+        "e": {
+        "eryk": "flaticon-boy-5",
+            "elf": "flaticon-elf-1"
+    },
+        "f": {
+        "farby": "flaticon-palette",
+            "fabryka": "flaticon-factory"
+
+    },
+        "g": {
+        "globus": "flaticon-earth-globe",
+            "gumka": "flaticon-eraser",
+            "gimbus": "flaticon-school-bus"
+    },
+        "i": {
+        "irlandia": "flaticon-leprechaun",
+    },
+        "j": {
+        "jabłko": "flaticon-apple",
+    },
+        "k": {
+        "książka": "flaticon-open-book ",
+            "kot": "flaticon-cat",
+            "krowa": "flaticon-cow",
+            "kura": "flaticon-hen",
+            "królik": "flaticon-rabbit",
+            "kobra": "flaticon-cobra",
+            "korona": "flaticon-crown"
+    },
+        "l": {
+        "lew": "flaticon-lion",
+            "lama": "flaticon-llama",
+            "leniwiec": "flaticon-sloth"
+    },
+        "m": {
+        "mumia": "flaticon-mummy-head-with-opened-eyes-for-halloween",
+            "muzyka": "musical-pentagram-lines-with-music-notes",
+            "mama": "flaticon-people-1",
+            "myszka": "flaticon-computer-mouse",
+            "medal": "flaticon-medal",
+            "motyl": "flaticon-butterfly",
+            "małpa": "flaticon-monkey",
+            "mysz": "flaticon-mouse"
+    },
+        "n": {
+        "notes": "flaticon-notebook",
+            "nemo": "flaticon-clown-fish"
+    },
+        "o": {
+        "opona": "flaticon-truck-wheel",
+            "okulary": "flaticon-glasses",
+            "ołówek": "flaticon-pencil",
+            "owca": "flaticon-sheep"
+    },
+        "p": {
+        "plecak": "flaticon-backpack",
+            "piłka": "flaticon-basketball",
+            "pióro": "flaticon-fountain-pen",
+            "pędzel": "flaticon-paint-brush",
+            "pies": "flaticon-bulldog",
+            "panda": "flaticon-panda",
+            "pelikan": "flaticon-pelican",
+            "pająk": "flaticon-spider"
+    },
+        "r": {
+        "rak": "flaticon-crab",
+            "rekin": "flaticon-shark"
+    },
+        "s": {
+        "sowa": "flaticon-owl",
+            "spinacz": "flaticon-paperclip",
+            "słoń": "flaticon-elephant",
+            "struś": "flaticon-ostrich",
+            "sklep": "flaticon-shop",
+            "smok": "flaticon-dragon"
+    },
+        "sz": {
+        "szkoła": "flaticon-school",
+            "szop": "flaticon-racoon"
+    },
+        "ś": {
+        "świnia": "flaticon-pig"
+    },
+        "t": {
+        "taczka": "flaticon-wheelbarrow",
+            "tablica": "flaticon-blackboard",
+            "teczka": "flaticon-briefcase",
+            "test": "flaticon-test",
+            "tygrys": "flaticon-tiger",
+            "tukan": "flaticon-toucan"
+    },
+        "w": {
+        "wąż": "flaticon-snake"
+    },
+        "z": {
+        "znak": "flaticon-stop",
+            "zamek": "flaticon-castle"
+    },
+        "ż": {
+        "żuk": "flaticon-beetle",
+            "żaba": "flaticon-frog",
+            "żyrafa": "flaticon-giraffe"
+    }
+    };
+
+    loadPage("m");
 });
+
+//988
