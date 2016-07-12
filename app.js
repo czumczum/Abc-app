605ba505fc2081cb5980c20ddcdf6a88753fa397
czumczum <e.mindjunk@gmail.com>
2016-07-12 10:45:36 +0200

    Completing the words array

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    //Variables
+    var showLetter;
+    var letters = document.querySelectorAll(".abc i");
+    var bigLetter = document.querySelector('h1.letter');
+    var count = 0;
+
+
+    //Functions
+    showLetter = function (e) {
+        if (letters[count].classList.contains('is-paused')){
+            letters[count].classList.remove('is-paused');
+        }
+        count++;
+        if (count == letters.length) {
+            bigLetter.removeEventListener("click", showLetter)
+        }
+    };
+    
+    bigLetter.addEventListener("click", showLetter);
+
+    for (var i = 0; i < letters.length; i++) {
+        console.log(letters[i].dataset.key);
+    }
+    
+    
+    //Database
+});
